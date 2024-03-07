25f9b8eaeda3c0f4c6326c6356fb31ef131f8bd3
guilhermeaalves <contatoguilhermealv@gmail.com>
2024-03-07 11:39:39 -0300

    Adição do modo claro

---
+++ assets/temas.js
@@ -0,0 +1,31 @@
+//DOM - Document Object Model - Modelo de Objeto do Documento
+//Transforma tudo na página em objetos. Cada objeto recebe atributos (propriedades) e métodos (funções) associadas a ele.
+
+//function "nome da função"(parâmetros)
+
+// Para qual tema?
+
+// Saber o tema atual e obter o elemento do tema atual
+
+// Saber onde está o atributo de tema
+
+// se o botão for clicado, então...
+document.querySelector('#btTrocaTema').addEventListener('click', () => {
+    // pegar o elemento com o tema 
+    // se o tema for light, então mude o atributo para dark
+    // se não for, então mude o atributo para light
+
+    if (document.documentElement.getAttribute('data-tema') === 'light') {
+        document.documentElement.setAttribute('data-tema', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-tema', 'light');
+    }   
+}) 
+
+
+
+
+// Trocar o tema:
+// 1. Pegar o elemento com tema
+// 2. Apagar o atributo do tema atual
+// 3. Escrever o atributo do novo tema
